c8b6f6329838f944edfc5defed87c5f7f6fb21fb
Android-Mu <yun12712@163.com>
2016-12-20 16:18:26 +0800

    修改查看按钮高度

--- app/src/main/java/com/example/mjj/flipoverdemo/MainActivity.java
+++ app/src/main/java/com/example/mjj/flipoverdemo/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
